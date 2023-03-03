9d4ab8fb34b1ca4e06616b54944e86036cd338bd
KianaMsqt <masqati.kiana@gmail.com>
2023-03-03 19:44:39 +0000

    Add links of all pages into header navlink

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// Add react-router
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Add bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from './components/Header';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Projects from './components/pages/Projects';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Header />
+        <div>
+            {/* Wrap Route elements in a Routes component */}
+            <Routes>
+                {/* Define routes using the Route component to render different page components at different paths */}
+                {/* Define a default route that will render the Home component */}
+                <Route path="/" element={<Home />} />
+                <Route path="/" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/projects" element={<Projects />} />
+            </Routes>
+        </div>
+    </Router>
   );
 }
 
