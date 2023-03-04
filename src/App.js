013c5d40b9c6621b8c33ca49ba13dc34ae6fd415
KianaMsqt <masqati.kiana@gmail.com>
2023-03-04 13:17:56 +0000

    - Remove useless div around Routes - Add Portfolio element

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from './components/Header';
 import About from './components/About';
-import Projects from './components/pages/Projects';
+import Portfolio from './components/Portfolio';
 import Contact from './components/pages/Contact';
 
 function App() {
@@ -15,67 +15,14 @@ function App() {
     <Router>
 		<Header />
 
-		<div>
-            {/* Wrap Route elements in a Routes component */}
-            <Routes>
-                {/* Define routes using the Route component to render different page components at different paths */}
-                {/* Define a default route that will render the Home component */}
-                <Route path="/" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/projects" element={<Projects />} />
-            </Routes>
-        </div>
-
-		<section id="portfolio">
-			<div className="container mt-5 mb-5 pt-5 pb-5">
-				<h2>My Portfolio</h2>
-				<div className="row gx-4 justify-content-between">
-
-					<div className="col-lg-3 col-md-6 mt-4 d-flex align-items-stretch">
-						<div className="card">
-							{/* <img className="card-img-top" src="https://placehold.co/600x400" alt="Card image cap" /> */}
-							<div className="card-body d-flex flex-column">
-								<h5 className="card-title">Portfolio Sample</h5>
-								<p className="card-text">This is a portfolio website created only with HTML and CSS.</p>
-								{/* <a href="https://kianamsqt.github.io/portfolio/" target="_blank" className="btn btn-dark mt-auto">Demo</a> */}
-							</div>
-						</div>
-					</div>
-					<div className="col-lg-3 col-md-6 mt-4 d-flex align-items-stretch">
-						<div className="card">
-							{/* <img className="card-img-top" src="https://placehold.co/600x400" alt="Card image cap" /> */}
-							<div className="card-body d-flex flex-column">
-								<h5 className="card-title">Flashlight</h5>
-								<p className="card-text">This is a flashlight effect following mouse cursor.</p>
-								{/* <a href="https://kianamsqt.github.io/flashlight-effect/" target="_blank" className="btn btn-dark mt-auto">Demo</a> */}
-							</div>
-						</div>
-					</div>
-					<div className="col-lg-3 col-md-6 mt-4 d-flex align-items-stretch">
-						<div className="card">
-							{/* <img className="card-img-top" src="https://placehold.co/600x400" alt="Card image cap" /> */}
-							<div className="card-body d-flex flex-column">
-								<h5 className="card-title">Front-EndEnd Triology</h5>
-								<p className="card-text">This is pre-work challenge of Front-EndEnd Triology bootcamp.</p>
-								{/* <a href="https://kianamsqt.github.io/prework-study-guide/" target="_blank" className="btn btn-dark mt-auto">Demo</a> */}
-							</div>
-						</div>
-					</div>
-					<div className="col-lg-3 col-md-6 mt-4 d-flex align-items-stretch">
-						<div className="card">
-							{/* <img className="card-img-top" src="https://placehold.co/600x400" alt="Card image cap" /> */}
-							<div className="card-body d-flex flex-column">
-								<h5 className="card-title">Module One</h5>
-								<p className="card-text">This is my work on module one of the bootcamp.</p>
-								{/* <a href="https://kianamsqt.github.io/module-1-challenge/" target="_blank" className="btn btn-dark mt-auto">Demo</a> */}
-							</div>
-						</div>
-					</div>
-				
-				</div>
-				
-			</div>
-		</section>
+		{/* Wrap Route elements in a Routes component */}
+		<Routes>
+			{/* Define routes using the Route component to render different page components at different paths */}
+			{/* Define a default route that will render the Home component */}
+			<Route path="/" element={<About />} />
+			<Route path="/portfolio" element={<Portfolio />} />
+			<Route path="/contact" element={<Contact />} />
+		</Routes>
 
 		<section id="skills" className="bg-dark text-white">
 			<div className="container mt-5 mb-5 pt-5 pb-5">
