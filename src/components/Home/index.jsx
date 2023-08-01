3a95c1458efd26645928d4f90613bd13819b48bb
KianaMsqt <masqati.kiana@gmail.com>
2023-08-01 10:17:49 +0100

    Update CV file

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,24 +1,41 @@
 import React from "react";
 import "./Home.css";
-import CV from "./Tayebe-Kiana-Masqati-resume.pdf";
+import CV from "./TayebeKianaMasqatiCV.pdf";
 
 function Home() {
   return (
-	<>
-		<div className="jumbotron jumbotron-fluid" id="about">
-			<div className="container">
-				<h1 className="display-4 text-white">About Me</h1>
-				<div className="lead">
-					<span className="text-white">Product enthusiast with 7+ years of experience in working with agile teams. Skilled in Front-End development. Reliable team player and eager to learn product skills.</span>
-					<ul>
-						<li className="text-white">Product Management Methodologies and Frameworks: Agile, Waterfall, Scrum</li>
-						<li className="text-white">Development Skills: HTML, CSS, Javascript, jQuery, Vue.js, Git, SCSS, Stylus, Mustache, Pug, WordPress</li>
-					</ul>
-					<a href={CV} title="Download Resume" download="Tayebe-Kiana-Masqati-resume.pdf" className="btn btn-primary">Download My Resume</a>
-				</div>
-			</div>
-		</div>
-	</>
+    <>
+      <div className="jumbotron jumbotron-fluid" id="about">
+        <div className="container">
+          <h1 className="display-4 text-white">About Me</h1>
+          <div className="lead">
+            <span className="text-white">
+              Product enthusiast with 7+ years of experience in working with
+              agile teams. Skilled in Front-End development. Reliable team
+              player and eager to learn product skills.
+            </span>
+            <ul>
+              <li className="text-white">
+                Product Management Methodologies and Frameworks: Agile,
+                Waterfall, Scrum
+              </li>
+              <li className="text-white">
+                Development Skills: HTML, CSS, Javascript, jQuery, Vue.js, Git,
+                SCSS, Stylus, Mustache, Pug, WordPress
+              </li>
+            </ul>
+            <a
+              href={CV}
+              title="Download Resume"
+              download="TayebeKianaMasqatiCV.pdf"
+              className="btn btn-primary"
+            >
+              Download My Resume
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
